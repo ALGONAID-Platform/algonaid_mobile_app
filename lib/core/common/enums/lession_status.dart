3925c95d31b1cd0d29387c1099071560d99a60ed
bashar galal esmail <159408410+BashargalalAlhimyari@users.noreply.github.com>
2026-04-23 06:42:35 -0700

    add lison status enum

---
+++ lib/core/common/enums/lession_status.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+import 'package:algonaid_mobail_app/core/common/extensions/theme_helper.dart';
+import 'package:flutter/material.dart';
+
+enum LessonStatus { notStarted, inProgress, completed, locked }
